1c82e85f2aa53f4d1aebd9d2733a909dac1b39aa
aiden <arutter@utk.edu>
2019-04-20 16:08:37 -0400

    Frontend work, map renders, just need to replace openstreetmap leaflets with our own

---
+++ js/leaflet.js
@@ -0,0 +1,26 @@
+/*
+ the script mus be loaded after the map div is defined.
+ otherwise this will not work (we would need a listener to
+ wait for the DOM to be fully loaded).
+
+ Just put the script tag below the map div.
+
+ The source code below is the example from the leaflet start page.
+ */
+
+var map = L.map('map', {
+    fullscreenControl: {
+        pseudoFullscreen: true // if true, fullscreen to page width and height
+    }
+}).setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+        
+map.toggleFullscreen(); // Either go fullscreen, or cancel the existing fullscreen.
+
