2463fc3f2eda4e56b77e5346f7f76cfbc5e1cb77
cadkin <cadkin17@vols.utk.edu>
2019-04-23 20:18:31 -0400

    Some small changes to account for daylight. Also discovered that on_chunk_charted handles almost all kinds of tile placement, as long as the player witnessed it. This is the same as it would work in game, so nothing lost there.

--- mod/control.lua
+++ mod/control.lua
@@ -1,21 +1,42 @@
 require("lib.screenshot")
 
-g_seed = 0
-
 -- On init, read the map seed and use it to store our map data.
 script.on_init(
     function()
-        g_seed = game["default_map_gen_settings"]["seed"]
+        global.g_seed = game["default_map_gen_settings"]["seed"]
+
+        -- If the number of ticks in the game is greater than 600 (10 seconds), then this must be a pre-existing save. Screenshot everything.
+        if game.tick > 600 then
+            sshot_all()
+        end
     end
 )
 
 -- When a chunk is charted, save a screenshot of it.
 script.on_event({defines.events.on_chunk_charted},
     function(e)
-        game.write_file("mapdata/" .. g_seed .. "/socket/chunks.socket", "CHARTED " .. e.position.x .. " " .. e.position.y .. "\n", true)
+        game.write_file("mapdata/" .. global.g_seed .. "/socket/chunks.socket", "CHARTED " .. e.position.x .. " " .. e.position.y .. "\n", true)
+
+        -- Save the time of day so we can restore it later.
+        local time = game.surfaces["nauvis"].daytime
+        game.surfaces["nauvis"].daytime = 0
+
         sshot_chunk(e.position)
+
+        game.surfaces["nauvis"].daytime = time
     end
 )
+
+-- When something is built, save a screenshot of it.
+--[[
+script.on_event({defines.events.on_built_entity},
+    function(e)
+        --game.write_file("mapdata/" .. global.g_seed .. "/socket/chunks.socket", "BUILT " .. e..x .. " " .. e.position.y .. "\n", true)
+        game.write_file("mapdata/" .. global.g_seed .. "/socket/chunks.socket", "BUILT\n", true)
+        --sshot_chunk(e.position)
+    end
+)
+--]]
 
 -- Handle the player position data.
 script.on_nth_tick(60,
@@ -23,7 +44,7 @@ script.on_nth_tick(60,
         -- For each player, update their position and write their positions to a file.
         for index, player in pairs(game.connected_players) do
             --player.print(player.position.x.." "..player.position.y);
-            game.write_file("mapdata/" .. g_seed .. "/socket/player.socket", "PLAYER " .. player.name .. " "  .. player.position.x .. " " .. player.position.y .. "\n", true);
+            game.write_file("mapdata/" .. global.g_seed .. "/socket/player.socket", "PLAYER " .. player.name .. " "  .. player.position.x .. " " .. player.position.y .. "\n", true);
         end
     end
 )
--- mod/lib/screenshot.lua
+++ mod/lib/screenshot.lua
@@ -8,16 +8,21 @@ function sshot_chunk(chunk)
     local chunk_y = chunk.y * tiles_per_chunk
 
     -- Add 16 to get center of chunk for screenshot.
-    game.take_screenshot({position = {chunk_x + 16, chunk_y + 16}, resolution = {chunk_res, chunk_res}, path = "mapdata/" .. g_seed .. "/c" .. chunk.x .. "_" ..chunk.y .. ".jpg", show_gui = false })
+    game.take_screenshot({position = {chunk_x + 16, chunk_y + 16}, resolution = {chunk_res, chunk_res}, path = "mapdata/" .. global.g_seed .. "/c" .. chunk.x .. "_" ..chunk.y .. ".jpg", show_gui = false })
 end
 
+-- Function that screenshots all generated chunks.
 function sshot_all()
+    local time = game.surfaces["nauvis"].daytime
+    game.surfaces["nauvis"].daytime = 0
+
+    -- 'nauvis' is the default surface in the game (the one the player starts on)
     for chunk in game.surfaces["nauvis"].get_chunks() do
         if game.surfaces["nauvis"].is_chunk_generated(chunk) then
-            game.write_file("chunks.log", "LOADED " .. chunk.x .. " " .. chunk.y .. "\n", true)
+            game.write_file("mapdata/" .. global.g_seed .. "/socket/chunks.socket", "LOADED " .. chunk.x .. " " .. chunk.y .. "\n", true)
             sshot_chunk(chunk)
         end
     end
 
-    game.set_wait_for_screenshots_to_finish()
+    game.surfaces["nauvis"].daytime = time
 end
