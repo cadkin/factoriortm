259d6d06f683652330a000d4792c09f3000145c1
aiden <arutter@utk.edu>
2019-04-24 17:25:07 -0400

    Deleted leaflet and refactor to web folder

---
+++ web/js/socket.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+// TODO: NO hardcoding paths
+var chunk_watcher = fs.watch("/home/solsane/.factorio/mapdata/1234567890/socket/chunk.socket");
+var chunk_watcher = fs.watch("/home/solsane/.factorio/mapdata/1234567890/socket/player.socket");
