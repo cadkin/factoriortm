684995d772918ed53e82d6f9c6d227e5b1495f53
aiden <arutter@utk.edu>
2019-04-24 20:30:36 -0400

    Potential fixes to script

--- factoriomap.py
+++ factoriomap.py
@@ -25,17 +25,37 @@ Usage: python3 factoriomap.py [source] [destination]
 '''
 class EventHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        print("event with file {}, type {}".format(event.src_path, event.event_type))
-        # Perform the modification to the changed file
-        chunk_to_tiles(event.src_path)
-        for zoom in range(9, 0, -1):
-            tiles = sorted(
-                glob('{}{}/*/*.jpg'.format(sys.argv[2], zoom+1)),
-                key=tile_coordinates)
-            for tile in tqdm(tiles):
-                zoom_out(tile, zoom)
+        if event.isfile:
+            print("event with file {}, type {}".format(event.src_path, event.event_type))
+            # Perform the modification to the changed file
+            chunk_to_tiles(event.src_path)
+            for zoom in range(9, 0, -1):
+                tiles = sorted(
+                    glob('{}{}/*/*.jpg'.format(sys.argv[2], zoom+1)),
+                    key=tile_coordinates)
+                for tile in tqdm(tiles):
+                    zoom_out(tile, zoom)
+    
+    def on_created(self, event):
+        if event.isfile:
+            print("event with file {}, type {}".format(event.src_path, event.event_type))
+            # Perform the modification to the changed file
+            chunk_to_tiles(event.src_path)
+            for zoom in range(9, 0, -1):
+                tiles = sorted(
+                    glob('{}{}/*/*.jpg'.format(sys.argv[2], zoom+1)),
+                    key=tile_coordinates)
+                for tile in tqdm(tiles):
+                    zoom_out(tile, zoom)
+        
 
 def main():
+    """Main executable function."""
+    # Verify arguments; print usage on failure.
+    if len(sys.argv) < 3 or not os.path.isdir(sys.argv[2]):
+        print(USAGE)
+        sys.exit()
+
     if os.path.isfile(sys.argv[1]):
         # If tar file
         if sys.argv[1].split('.')[-1] == 'tar':
@@ -46,8 +66,10 @@ def main():
 
         else:        # singular file
             chunk_to_tiles(sys.argv[1])
-
     else:
+        if not os.path.exists(sys.argv[1]):
+            os.makedirs(sys.argv[1])
+
         chunks = sorted(glob(sys.argv[1]+'chunk_*.jpg'), key=chunk_coordinates)
         for chunk in tqdm(chunks):
             chunk_to_tiles(chunk)
@@ -59,12 +81,6 @@ def main():
         for tile in tqdm(tiles):
             zoom_out(tile, zoom)
     
-    """Main executable function."""
-    # Verify arguments; print usage on failure.
-    if len(sys.argv) < 3 or not os.path.isdir(sys.argv[2]):
-        print(USAGE)
-        sys.exit()
-
     observer = Observer()
     event_handler = EventHandler()
 
